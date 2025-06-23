d80d6db3902619d4964ede2de1a04ae8192d7afe
Greeceuniquecode <notgreece2@gmail.com>
2025-06-23 18:18:12 +0545

    loops in javascript(js)

---
+++ loops.js
@@ -0,0 +1,19 @@
+
+// let sum = 0
+
+// for (i = 1; i <= 10; i++) {
+//     console.log(i)
+//     sum = sum + i;
+// }
+
+// console.log(sum);
+ i = 1
+while (i<=10) {
+ console.log(i,);
+ 
+    i++
+
+    
+    
+}
+
