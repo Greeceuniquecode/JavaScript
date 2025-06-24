aff3be763cb2dcd4b2f3b67ce516d71922339c8c
Greeceuniquecode <notgreece2@gmail.com>
2025-06-24 17:41:57 +0545

    functions in js

---
+++ function.js
@@ -0,0 +1,19 @@
+function DisplayHelloWorld() {
+    console.log('hello world');
+}
+for (i = 1; i <= 10; i++) {
+    DisplayHelloWorld();
+}
+function sum(num1, num2) {
+    result = num1 + num2
+    return result;
+}
+result = sum(5, 6)
+console.log(result);
+function myName() {
+    myname = "Greece Dahal"
+    return myname;
+}
+myname = myName();
+console.log(myname);
+
