d1dd884cb623f40a28472449a2884029aa667916
Greeceuniquecode <notgreece2@gmail.com>
2025-06-24 18:04:06 +0545

    list and its functions

---
+++ list.js
@@ -0,0 +1,31 @@
+// //list should be enclosed by Square Brackets
+// //both strings and integers can be used
+list = [1,3,2,4,5,'ok']
+// console.log(list);
+// list2 = ["hello",2,3,4,5]
+// console.log(list2);
+// console.log(list+list2);//we can add list
+// list.push('javascript') //push function is used of adding new value to the list
+// console.log(list);
+// list.pop();//pop function removes the last value of the list
+// console.log(list);
+// console.log(list[5]);
+
+// list.forEach(a => {
+//     console.log(a);
+// });
+
+// list.map((item)=>{
+// console.log(item);
+// });
+list.reverse()
+console.log(list);
+list.sort()
+console.log(list);
+list.sort().reverse()
+console.log(list);
+
+
+
+
+
