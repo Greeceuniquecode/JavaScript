5a53806d3bc7558e195119451992b5c5d271369b
Greeceuniquecode <notgreece2@gmail.com>
2025-06-24 18:32:05 +0545

    split and join function

---
+++ Strings.js
@@ -0,0 +1,10 @@
+myname = 'greece dahal'
+spliteddata= myname.split(" ")
+console.log(spliteddata);
+spliteddata.map((element)=>{
+    console.log(element);
+});
+joineddata=spliteddata.join(" ");
+
+console.log(joineddata);
+
