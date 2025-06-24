322f7c968e84fe427a210f459a596b35ff0ac551
Greeceuniquecode <notgreece2@gmail.com>
2025-06-24 18:42:45 +0545

    objects in js

---
+++ objects.js
@@ -0,0 +1,17 @@
+obj={
+    "name":'Greece Dahal',
+    "address":"USA",
+    "age":18,
+    "contactno":980909090
+}
+// console.log(obj.name);//accessing single value of object
+console.log('your name is',obj.name,
+    'your address is ',obj.address,
+    'you are',obj.age,"years old",
+    'your contact number is',obj.contactno,
+);
+console.log(Object.keys(obj));
+console.log(Object.values(obj));
+
+
+
