a5dfcf70b69c659d0f4de60c7faa474b926acc7d
Greeceuniquecode <notgreece2@gmail.com>
2025-06-25 17:34:18 +0545

    objects in javascript

--- objects.js
+++ objects.js
@@ -1,17 +1,31 @@
-obj={
-    "name":'Greece Dahal',
-    "address":"USA",
-    "age":18,
-    "contactno":980909090
+obj = {
+    "name": 'Greece Dahal',
+    "address": "USA",
+    "age": 18,
+    "contactno": 980909090
 }
-// console.log(obj.name);//accessing single value of object
-console.log('your name is',obj.name,
-    'your address is ',obj.address,
-    'you are',obj.age,"years old",
-    'your contact number is',obj.contactno,
+console.log(obj.name);//accessing single value of object
+console.log('your name is', obj.name,
+    'your address is ', obj.address,
+    'you are', obj.age, "years old",
+    'your contact number is', obj.contactno,
 );
 console.log(Object.keys(obj));
 console.log(Object.values(obj));
-
-
+users = [
+    {
+        "name": 'Suman Regmi',
+        "email": "hey123@gmail.com",
+        "address": 'baliya',
+    },
+    {
+        "name": 'Greece Dhakal',
+        "email": 'notgrish2@gmail.com',
+        'address': 'Greece'
+    }
+]
+// console.log(users);
+users.map((user)=>{
+    console.log(user.name);
+})
 
--- variables.js
+++ variables.js
@@ -3,5 +3,9 @@ num2=4
 console.log('the sum of ',num1,'and ',num2,'is',num1+num2); //string concat
 let a;
 var b;
+a=10
 console.log(a)
 console.log(b)
+
+const c=1;
+console.log(c);
